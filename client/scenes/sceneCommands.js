4c6eba3279fb6c99550082f9d57d7f818f64c30a
Gavin McDermott <gavinmcdermott@gmail.com>
2016-08-31 16:55:19 -0700

    add solar scene nodes

--- client/scenes/sceneCommands.js
+++ client/scenes/sceneCommands.js
@@ -2,37 +2,37 @@ import { non, noff, ncap, con, coff, overlay, intro, activeScene, sceneCaption, 
 import { RESTING, MESSAGING } from './../constants/constants'
 
 export let sceneCommands = [
-	
-	overlay(true),
-	intro(true),
-	pause(1),
+
+	// overlay(true),
+	// intro(true),
+	// pause(1),
 	intro(false),
-	pause(1),
+	// pause(1),
 	overlay(false),
 
-	sceneCaption("Don't make a scene"),
+	// sceneCaption("Don't make a scene"),
 
-	ncap(0, "I'm about to send a message"),
-	pause(1),
-	[ non(0), con('0-6') ],
-	[ noff(0), ncap(0, null) ],
-	[ non(6), delay(ncap(6, 'I just got a message'), 0.2), coff('0-6') ],
-	pause(2),
-	ncap(6, "Now I'm going to send a message"),
-	sceneCaption(),
-	pause(0.5),
-	sceneCaption('Ok, this is a different caption'),
-	noff(0),
-	pause(1),
-	con('6-3'),
-	coff('6-3'),
-	[ non(3), delay(ncap(3, 'Now I got the message, wow! This is great'), 0.2) ],
-	[ noff(6), ncap(6, null) ],
+	// ncap(0, "I'm about to send a message"),
+	// pause(1),
+	// [ non(0), con('0-6') ],
+	// [ noff(0), ncap(0, null) ],
+	// [ non(6), delay(ncap(6, 'I just got a message'), 0.2), coff('0-6') ],
+	// pause(2),
+	// ncap(6, "Now I'm going to send a message"),
+	// sceneCaption(),
+	// pause(0.5),
+	// sceneCaption('Ok, this is a different caption'),
+	// noff(0),
+	// pause(1),
+	// con('6-3'),
+	// coff('6-3'),
+	// [ non(3), delay(ncap(3, 'Now I got the message, wow! This is great'), 0.2) ],
+	// [ noff(6), ncap(6, null) ],
 
-	overlay(true),
-	pause(1),
-	activeScene(1),
-	overlay(false)
+	// overlay(true),
+	// pause(1),
+	// activeScene(1),
+	// overlay(false)
 
 
 
@@ -42,13 +42,13 @@ export let sceneCommands = [
 	// [ non(6), ncap(6, 'project is on schedule') ],
 	// [ con('6-3'), con('6-4'), con('6-5') ],
 	// [ coff('6-3'), coff('6-4'), coff('6-5') ],
-	// [ non(3), 
-	// 	non(4), 
-	// 	non(5), 
-	// 	ncap(3, 'project budget decreased'), 
-	// 	delay(ncap(4, 'engineers notified new beams coming'), 0.2), 
+	// [ non(3),
+	// 	non(4),
+	// 	non(5),
+	// 	ncap(3, 'project budget decreased'),
+	// 	delay(ncap(4, 'engineers notified new beams coming'), 0.2),
 	// 	delay(ncap(5, 'crane company scheduling vehicles'), 0.5)]
-	
+
 
 	//end of scene 0
 
@@ -57,10 +57,10 @@ export let sceneCommands = [
 	// introOn(1)
 	// setScene(1)
 	// pause(5)
-	// [ introOff(1), overlayOff()] 
-	// 
+	// [ introOff(1), overlayOff()]
+	//
 
-	
+
 
 
 
