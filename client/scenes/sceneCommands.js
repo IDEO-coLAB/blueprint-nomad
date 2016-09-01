3052de3fba98df9f2ea8963cf257789648b2e2ab
Gavin McDermott <gavinmcdermott@gmail.com>
2016-09-01 16:22:50 -0700

    pre merge

--- client/scenes/sceneCommands.js
+++ client/scenes/sceneCommands.js
@@ -9,15 +9,15 @@ export let sceneCommands = [
 
 	// Turn on solar array
 	ncap(0, 'Lots of sun here'),
-	pause(1),
+	non(0),
+	pause(),
 	ncap(1, 'Lots of sun here, too'),
-	[ non(0), non(1) ],
+	non(1),
+	pause(),
 
 	// ping
 	[ con('0-2'), con('1-2') ],
 	[ coff('0-2'), coff('1-2') ],
-
-	pause(),
 
 	[ noff(0), ncap(0, null), noff(1), ncap(1, null) ],
 	[ non(2), ncap(2, 'Plenty of sun in the region') ],
