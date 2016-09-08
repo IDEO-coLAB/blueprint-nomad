9bd9938cebcf0544cbff8f0020a19ac1422ed105
Reid Williams <rwilliams@ideo.com>
2016-09-08 10:27:31 -0700

    wip

--- client/components/NodeComponent.js
+++ client/components/NodeComponent.js
@@ -1,21 +1,17 @@
+
 import React, {Component} from 'react'
 import R from 'ramda'
 
-import { RESTING, MESSAGING, ALERT } from './../constants/constants'
+import { RESTING, MESSAGING } from './../constants/constants'
 import SpeechBubbleComponent from './SpeechBubbleComponent'
-import { NODE_ACTIVATE_TIMEOUT, NODE_DEACTIVATE_TIMEOUT } from './../constants/settings'
 
 let speechBubbleOffset = { x: 50, y: -50 }
 
 class NodeComponent extends Component {
   render() {
-    if (this.props.id === 'energyPrediction') {
-      console.log(this.props)
-    }
-
     let styleMessaging = {
       fill: "#53BCCF",
-      transition: 'fill 1.0s'
+      transition: 'fill 0.5s'
     }
 
     let styleResting = {
@@ -23,39 +19,30 @@ class NodeComponent extends Component {
       transition: 'fill 1.0s'
     }
 
-    let styleAlert = {
-      fill: "#ff0000",
-      transition: 'fill 1.0s'
-    }
+    let bubbleX = this.props.x + speechBubbleOffset.x
+    let bubbleY = this.props.y + speechBubbleOffset.y
 
-  	let bubbleX = this.props.pos.x + speechBubbleOffset.x
-  	let bubbleY = this.props.pos.y + speechBubbleOffset.y
-
-    let style = styleResting
-  	if (R.equals(this.props.state, MESSAGING)) { style = styleMessaging }
-    // if (R.equals(this.props.status, ALERT)) { style = styleAlert }
-
+    let style = (R.equals(this.props.state, MESSAGING))? styleMessaging : styleResting
     return (
-    	<g>
-    		<SpeechBubbleComponent x={bubbleX} y={bubbleY} width="100" height="50" text={this.props.captionText} visible={this.props.caption}/>
-	    	<g onClick={() => {console.log(123) }} >
+      <g>
+        <SpeechBubbleComponent x={bubbleX} y={bubbleY} width="100" height="50" text={this.props.captionText} visible={this.props.caption}/>
+        <g onClick={() => { console.log(this.props.id)}} >
           <circle 
             style={style} 
             cx={this.props.pos.x} 
             cy={this.props.pos.y} 
-            r={this.props.pos.rad}>
-          </circle>
-		    </g>
-		  </g>
+            r="40" />
+         </g>
+      </g>
     )
   }
 }
 
 let renderSpeechBubble = (text, x, y) => {
-	if (R.isEmpty(text) || R.isNil(text)) { return }
-	return (
-		<SpeechBubbleComponent x={x} y={y} width="100" height="50" text={text}/>
-	)
+  if (R.isEmpty(text) || R.isNil(text)) { return }
+  return (
+    <SpeechBubbleComponent x={x} y={y} width="100" height="50" text={text}/>
+  )
 }
 
 module.exports = NodeComponent
