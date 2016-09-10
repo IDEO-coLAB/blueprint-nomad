eb4dd3c6f7fee18624ad8298da24a8ea9b5a5b56
Reid Williams <rwilliams@ideo.com>
2016-09-09 19:01:11 -0700

    added floating speech bubbles

--- client/components/NodeComponent.js
+++ client/components/NodeComponent.js
@@ -1,6 +1,7 @@
 
 import React, {Component} from 'react'
 import R from 'ramda'
+import perlin from 'perlin-noise'
 
 import { RESTING, MESSAGING } from './../constants/constants'
 import SpeechBubbleComponent from './SpeechBubbleComponent'
@@ -8,6 +9,24 @@ import NodeIconComponent from './NodeIconComponent'
 
 let speechBubbleOffset = { x:40, y:-80 }
 let nodeIconOffset = { x:-150, y:40 }
+
+let speechOffsetNoise = perlin.generatePerlinNoise(
+  1e6, 1, { octaveCount: 10, amplitude: 0.1, persistence: 0.1 })
+
+let getSpeechBubblePos = (i) => {
+  let xindx = i % R.length(speechOffsetNoise)
+  let xnoise = speechOffsetNoise[xindx]
+
+  let yindx = (i + 10000) % R.length(speechOffsetNoise)
+  let ynoise = speechOffsetNoise[yindx]
+
+  let offset = {
+    x: xnoise * 50,
+    y: ynoise * 50
+  }
+
+  return offset
+}
 
 let renderSpeechBubble = (node, x, y) => {
   return <SpeechBubbleComponent x={x} y={y} width="100" height="50" text={node.captionText} visible={node.caption} />
@@ -35,6 +54,18 @@ let renderNode = (node, style) => {
 }
 
 class NodeComponent extends Component {
+  constructor() {
+    super()
+
+    // start with random so components don't dance in sync
+    this.floatTimer = Math.floor(Math.random() * 500)
+    let self = this
+    setInterval(() => {
+      self.floatTimer++
+      self.forceUpdate()
+    }, 20)
+  }
+
   render() {
 
     const compositeStroke = '#3399FF'
@@ -57,8 +88,9 @@ class NodeComponent extends Component {
       transition: 'fill 1.0s'
     }
 
-    let bubbleX = this.props.pos.x + speechBubbleOffset.x
-    let bubbleY = this.props.pos.y + speechBubbleOffset.y
+    let noiseOffset = getSpeechBubblePos(this.floatTimer)
+    let bubbleX = this.props.pos.x + speechBubbleOffset.x + noiseOffset.x
+    let bubbleY = this.props.pos.y + speechBubbleOffset.y + noiseOffset.y
 
     let iconX = this.props.pos.x + nodeIconOffset.x
     let iconY = this.props.pos.y + nodeIconOffset.y
