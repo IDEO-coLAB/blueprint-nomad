73836c6bc60014406bd461bd4bb7ce20c3d11665
Reid Williams <rwilliams@ideo.com>
2016-09-11 11:34:10 -0700

    left mouse click advances scene

---
+++ client/components/MouseInputComponent.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+import R from 'ramda'
+
+class MouseInputComponent extends Component {
+  render() {
+    let style = {
+      position: 'absolute',
+      left: 0,
+      top: 0,
+      width: 1620,
+      height: 1080,
+      zIndex: 1,
+      opacity: 0
+    }
+
+    let self = this
+
+    return (
+    	<div style={style} onClick={event => self.props.mouseClicked(event)}></div>
+    )
+  }
+}
+
+module.exports = MouseInputComponent
--- client/containers/AppContainer.js
+++ client/containers/AppContainer.js
@@ -2,6 +2,14 @@ import React, {Component} from 'react'
 import { connect } from 'react-redux'
 import R from 'ramda'
 
+import MouseInputComponent from './../components/MouseInputComponent'
+import TitleScene from './../containers/TitleScene'
+import WhatisNomadScene from './../containers/WhatisNomadScene'
+import ImagineScene from './../containers/ImagineScene'
+import Imagine2Scene from './../containers/Imagine2Scene'
+import Imagine3Scene from './../containers/Imagine3Scene'
+import VoiceScene from './../containers/VoiceScene'
+import Voice2Scene from './../containers/Voice2Scene'
 import InteractiveMapScene from './../containers/InteractiveMapScene'
 
 function mapStateToProps(state, ownProps) {
@@ -15,9 +23,38 @@ function mapDispatchToProps(dispatch) {
 }
 
 class App extends Component {
+	constructor() {
+		super()
+		this.currentScene = 0
+		this.scenes = [
+			<TitleScene />,
+			<WhatisNomadScene />,
+			<ImagineScene />,
+			<Imagine2Scene />,
+			<Imagine3Scene />,
+			<VoiceScene />,
+			<Voice2Scene />,
+			<InteractiveMapScene />
+		]
+	}
+
+	nextScene() {
+		this.currentScene = (this.currentScene + 1) % R.length(this.scenes)
+		this.forceUpdate()
+	}
+
+	previousScene() {
+		this.currentScene = (this.currentScene - 1) % R.length(this.scenes)
+		this.forceUpdate()
+	}
+
   render() {
+  	let self = this
     return (
-      <InteractiveMapScene />
+    	<div>
+	    	<MouseInputComponent mouseClicked={(event) => { self.nextScene() }} />
+	      { this.scenes[this.currentScene] }
+      </div>
     )
   }
 }
